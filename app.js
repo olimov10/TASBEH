79fb3c9c97137ea487c33b408d588aa7973790ea
olimov10 <olimovi168@gmail.com>
2024-03-13 20:56:34 +0500

    Tasbeh

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector('.bos').addEventListener('click', function () {
+    a = document.querySelector('.ekran')
+    a.value = parseInt(a.value) + 1
+    console.log(a.value);
+})
+document.querySelector('.qayt').addEventListener('click', function () {
+    a = document.querySelector('.ekran')
+    ochirish = 0
+    a.value = ochirish
+    console.log(ochirish);
+})
